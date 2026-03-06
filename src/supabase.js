5172863d8c20e1c49ab7d056e510ca2d0406907e
badogguk-oss <badog.guk@gmail.com>
2026-03-06 17:17:02 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,6 @@
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://https://hvuymucimfzriqlfbwq.supabase.co.supabase.co'
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_yn67p4f2KC4BDJY1fETQDw_LVM5Q967'
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://hvuymucimfzriq1fbwq.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_yn67p4f2KC4BDJY1fETQDw_LVM5Q967'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
