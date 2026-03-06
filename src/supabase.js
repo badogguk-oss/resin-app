7d10280ff8510ae2c719909aa6edfddfc470ba76
badogguk-oss <badog.guk@gmail.com>
2026-03-06 17:30:08 +0800

    1Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://hvuymucimfzriq1fbwq.supabase.co'
+const SUPABASE_URL = 'https://hvuymucimfzriqlyfbwq.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_yn67p4f2KC4BDJY1fETQDw_LVM5Q967'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
