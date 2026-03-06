fa8620439933a4f17c972ebca8558accef275072
badogguk-oss <badog.guk@gmail.com>
2026-03-06 17:12:24 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Ganti dengan URL dan Key dari Supabase project kamu
-// Cara dapat: supabase.com → project → Settings → API
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://YOUR_PROJECT.supabase.co'
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'YOUR_ANON_KEY'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://https://hvuymucimfzriqlfbwq.supabase.co.supabase.co'
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_yn67p4f2KC4BDJY1fETQDw_LVM5Q967'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
